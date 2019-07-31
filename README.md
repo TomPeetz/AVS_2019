a4897fde1cd4a637f1023812796df1c03647badf
dennis-th <nitro_123@web.de>
2019-07-31 15:29:31 +0200

    README

--- README.md
+++ README.md
@@ -1,8 +1,19 @@
 # AVS_2019
 
-Required: openmpi
+Benötigt wird: openmpi, sumo, libpython
+
+Ebenfalls alle in requirements.txt genannten python packages
+
+Das script randomTrips.py von sumo in /usr/share/sumo/tools/
+
+Zuerst wird für die genutzte Karte der Search Space erzeugt
 
 ```
-pip3 install mpi4py
-mpiexec -np 4 python3 TrafficSimulation.py
+python GenerateSearchSpace.py
 ```
+Dies erzeugt die Datei searchspace.json, in der alle möglichen Variationen gelistet sind
+
+```
+mpiexec -np 3 python TrafficSimulation.py
+```
+Führt die Simulation mit 2 Workern aus
--- Src/GenerateSearchSpace.py
+++ Src/GenerateSearchSpace.py
@@ -1,17 +1,10 @@
-import ModMap
 import sumolib
 import json
+from os import path
 
-netxml = "/home/dennnis/avs/AVS_2019/Experimente/Netconvert_Mit_Python/Maps/Minimal/SUMO_Netz/minimal.net.xml"
+netxml = path.abspath(path.join("..", "..", "Experimente/Netconvert_Mit_Python/Maps/Minimal/SUMO_Netz/minimal.net.xml"))
 net = sumolib.net.readNet(str(netxml))
 
-#netconvert = ModMap.load_netconvert_binary()
-#print(netconvert)
-
-#tmpd, plain_files = ModMap.cnvt_net_to_plain(net_path = "/home/dennnis/avs/AVS_2019/Experimente/Netconvert_Mit_Python/Maps/Minimal/SUMO_Netz/minimal.net.xml",
-#                         netcnvt_bin = netconvert,
-#                         plain_output_prefix = "test",
-#                         verbose=True)
 nodes = net.getNodes()
 ranodes = []
 
@@ -39,5 +32,4 @@ json_data = json.dumps(searchspace)
 with open('searchspace.json', 'w') as f:
     f.write(json_data)
 
-#print(nodes)
 print(searchspace)
